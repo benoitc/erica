e1d7a060cca5f9476234c7ace9088917b72e51a0
Chris Anderson <jchris@grabb.it>
2009-01-08 23:11:53 -0800

    rake runs fm specs

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -2,6 +2,8 @@ require "rubygems"
 require "spec" # Satisfies Autotest and anyone else not using the Rake tasks
 require "couchrest"
 
+require File.expand_path(File.dirname(__FILE__)) + '/../lib/couchapp'
+
 COUCHAPP = ARGV[0] || File.expand_path(File.dirname(__FILE__)) + '/../bin/couchapp'
 SCRATCH_PATH = File.dirname(__FILE__) + '/scratch'
 COUCHHOST = "http://127.0.0.1:5984"
