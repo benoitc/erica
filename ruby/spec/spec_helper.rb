2aeeb3f13d19cb35e7cb01f0248e9bbf67a0716d
Chris Anderson <jchris@grabb.it>
2009-01-11 00:51:46 -0800

    test db name

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -7,7 +7,7 @@ require File.expand_path(File.dirname(__FILE__)) + '/../lib/couchapp'
 COUCHAPP = ARGV[0] || File.expand_path(File.dirname(__FILE__)) + '/../bin/couchapp'
 SCRATCH_PATH = File.dirname(__FILE__) + '/scratch'
 COUCHHOST = "http://127.0.0.1:5984"
-TESTDB = 'couchrest-test'
+TESTDB = 'couchapp-test'
 
 def reset_test_db!
   cr = CouchRest.new(COUCHHOST)
