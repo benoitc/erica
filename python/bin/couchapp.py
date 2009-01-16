fb4aa8cfd24b9ebbe25014ccf0cda0f469bbde3e
Dean Landolt <dean@hardy2.(none)>
2009-01-16 19:02:58 +0000

    refactored couchapp.py from janl's original

--- python/bin/couchapp.py
+++ python/bin/couchapp.py
@@ -14,11 +14,10 @@ Good for pure couch application development.
 A port of couchapp of `couchrest` (http://github.com/jchris/couchrest/)
 """
 
-import optparse
-import dircache
-import os
+import os, sys
+from dircache import listdir
+import urlparse
 
-from posixpath import isdir, realpath
 from md5 import md5
 
 try:
@@ -27,26 +26,17 @@ except ImportError:
     import json # Python 2.6
 from couchdb import __version__ as VERSION, client
 
+
 class InvalidApplicationNameException(Exception):
     """The current directory includes invalid characters"""
 
+
+# add a depth param and skip all startswith('_') at depth==0
+
+
 class Couchapp():
     """manages file hierarchies and design docs"""
-    mimes = {
-      "html" : "text/html",
-      "htm"  : "text/html",
-      "png"  : "image/png",
-      "gif"  : "image/gif",
-      "css"  : "text/css",
-      "js"   : "test/javascript",
-      "txt"  : "text/plain"
-    }
     
-    def mime(self, ext):
-        if ext in self.mimes:
-            return self.mimes[ext]
-        else:
-            return "unknown/" + ext
     # def get_appname(self):
     #     """Validates the current dir name against design doc name constraints"""
     #     appname = os.curdir
@@ -56,93 +46,140 @@ class Couchapp():
     #         raise InvalidApplicationNameException
     #     
     #     return appname
-
+    
     def __init__(self, dir):
         """Initializes app object"""
-        self.dir = realpath(dir)
-
-    def readfile(self, filename):
+        self.dir = os.path.realpath(dir)
+        self.attachments = []
+        self.files = {}
+    
+    def readfile(self, pathname):
         """Reads a file, returns a dict of content-type and content"""
         return {
-            "name": filename,
-            "content-type": self.mime(filename.split(".")[-1]), 
-            "content": file(filename).read()}
-
-    def readdir(self, dir):
-        print "reading dir " + dir;
+            "name": pathname,
+            "content-type": self.mime(pathname.split('.')[-1]), 
+            "content": file(pathname).read()}
+    
+    def get_files(self):
+        self.files = recurse_dirs(self.dir)
+    
+    def get_attachments(self):
+        if os.path.isdir(os.path.join(self.dir, '_attachments')):
+            for root, dirs, files in os.walk(os.path.join(self.dir, '_attachments')):
+                for name in files:
+                    path = os.path.join(root, name)
+                    path = path[path.find('_attachments/')+13:]
+                    self.attachments.append(path)
+    
+    def push(self, app_name, target):
+        """Pushes dir hierarchy to a design doc"""
         
-        files = dict() 
-        entries = dircache.listdir(dir)
-        for entry in entries:
-            if entry.startswith("."):
-                continue
-
-            if isdir(entry):
-                # handle dir
-                files[entry] = self.walk(dir + "/" + entry)
-            else:
-                print "reading file " + entry;
-                # handle file
-                files[entry] = self.readdir(dir + "/" + entry)
-        return files
-
-    def push(self, dbname):
-        """pushes dir hierarchy to a design doc"""
-
-        # if not target.startswith("http://") and 
-        #    not target.startswith("https://"):
-        #     target = "http://127.0.0.1:5984/" + target
+        # guess the database details
+        if target.startswith('http://') or target.startswith('https://'):
+            split = target.split('/')
+            server_name = '/'.join(split[:3])
+            db_name = '/'.join(split[3:])
+        else:
+            server_name = 'http://127.0.0.1:5984'
+            db_name = target
         
         # open target db
         #   create if it does not exist
-        server = client.Server("http://127.0.0.1:5984/")
-        if not dbname in server:
-            db = server.create(dbname)
+        server = client.Server(server_name)
+        if not db_name in server:
+            db = server.create(db_name)
         else:
-            db = server[dbname]
-
-        # create doc from dirhier
-        #   grab old doc, if exists
-        if  self.dir in db:
-            doc = db[self.dir]
-        else:
-            doc = client.Document
-
-        # read dir into a dict
-        pushfiles = self.readdir(self.dir)
-        pushfiles.signatures = {}
-        # calc md5s for _attachments
-        for attachment in pushfiles["_attachments"]:
-            pushfiles.signatures[attachment.name](md5(attachment.content))
-        # save or update doc
+            db = server[db_name]
         
-def main():
-    usage = '%prog command [dir] [target]'
+        
+        app_name = '_design/' + app_name
+        
+        if app_name in db:
+            db[app_name] = {'_rev': db[app_name].rev}
+        else:
+            doc = {}
+        doc
+        
+        # read objects recursively
+        self.get_files()
+        
+        # get _attachments
+        self.get_attachments()
+        
+        # calc md5s for _attachments
+        self.files['signatures'] = {}
+    #    for name, content in pushfiles['_attachments'].items():
+    #        pushfiles['signatures'][name] = md5(content).hexdigest()
+        
+        
+        db[app_name] = self.files
+        
+        for filename in self.attachments:
+            pathname = os.path.join(self.dir, '_attachments', filename)
+            #db.put_attachment(doc, file(pathname).read(), filename=filename)
 
-    parser = optparse.OptionParser(usage=usage, version=VERSION)
 
-    # parser.add_option('command',
-    #     action='store',
-    #     dest='command',
-    #     help='what command to run, see --help')
 
-# TODO: proper arg parsing
-
-    # options, arg = parser.parse_args()
-
-    options = dict()
-    options['command'] = "push"
-    options['dir'] = "."
-    options['target'] = "apptest"
-
-#
-
-    app = Couchapp(options['dir'])
     
-    if options['command'] == "push":
-        app.push(options['target'])
+def recurse_dirs(dir, depth=0):
+    """Iterates through application root and populates app members"""
+    entries = listdir(dir)
+    files = {}
+    
+    for entry in entries:
+        if entry.startswith('.'):
+            continue
+        elif entry == '_attachments':
+            continue
+        
+        elif os.path.isdir(os.path.join(dir, entry)):
+            # handle dir
+            files[entry] = recurse_dirs(os.path.join(dir, entry), depth+1)
+        
+        else:
+            # handle file
+            contents = file(os.path.join(dir, entry)).read()
+            if entry.endswith('.json'):
+                files[entry] = json.loads(contents)
+            elif entry.endswith('.js'):
+                files[entry[:-3]] = contents
+            else:
+                files[entry] = contents
+    
+    return files
 
-    # if options.command == "generate":
+def main():
+    # ghetto options parsing because I can't never figure out optparse
+    options = dict()
+    
+    if len(sys.argv) <= 1:
+        print 'usage: command [dir] [target] [name]'
+    else:
+        options['command'] = sys.argv[1]
+    
+    if len(sys.argv) >= 3:
+        options['dir'] = sys.argv[2]
+    else:
+        options['dir'] = '/home/dean/Projects/couchapp/app-template' # '/home/dean/Projects/sofa'
+    
+    if len(sys.argv) >= 4:
+        options['target'] = sys.argv[3]
+    else:
+        options['target'] = 'apptest'
+    
+    if len(sys.argv) >= 5:
+        options['name'] = sys.argv[4]
+    else:
+        options['name'] = options['dir'].split(os.path.sep)[-1]
+    
+    
+    app = Couchapp(options['dir'])
+    if options['command'] == 'test':
+        app.push('couchapp_test', 'apptest')
+    elif options['command'] == 'push':
+        app.push(options['name'], options['target'])
+    
+    # if options.command == 'generate':
     #     app.generate()
 
 if __name__ == '__main__':
