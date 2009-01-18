fd4058ca3e52036a0296f7d6bbf5ba52da62e64a
Chris Anderson <jchris@grabb.it>
2009-01-17 19:01:24 -0800

    license file name and couchapp url

--- python/couchapp/__init__.py
+++ python/couchapp/__init__.py
@@ -3,7 +3,7 @@
 #
 # Copyright 2009 Benoit Chesneau <benoitc@e-engura.org>
 #
-# This software is licensed as described in the file COPYING, which
+# This software is licensed as described in the file LICENSE, which
 # you should have received as part of this distribution.
 
 __VERSION__ = '0.1.4'
