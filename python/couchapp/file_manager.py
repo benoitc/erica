544b424072855bfc4fb1d97fd1867d1040379a7c
benoitc <benoitc@e-engura.org>
2009-01-18 01:19:01 +0100

    New version of couchapp. More pythonic. This a copy-feature of ruby version.

---
+++ python/couchapp/file_manager.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 Benoit Chesneau <benoitc@e-engura.org>
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+from glob import glob
+from mimetypes import guess_type
+import os
+import shutil
+import urlparse
+
+# compatibility with python 2.4
+try:
+    from hashlib import md5 as _md5
+except ImportError:
+    import md5
+    _md5 = md5.new
+
+try:
+    import simplejson as json
+except ImportError:
+    import json # Python 2.6
+
+
+from couchdb import Server
+
+class FileManager(object):
+    
+    def __init__(self, dburl):
+        # split dburl
+        parts = urlparse.urlsplit(dburl)
+        if parts[0] != 'http' and parts[0] != 'https':
+            raise ValueError('Invalid dburl')
+        
+        db_path = parts[2].strip('/').split('/')
+        self.db_name = db_path[0]
+        
+        self.server_uri = '%s://%s' % (parts[0], parts[1])
+        self.couchdb_server = Server(self.server_uri)
+
+        # create db if it don't exist
+        try:
+            self.db = self.couchdb_server.create(self.db_name)
+        except: # db already exist
+            self.db = self.couchdb_server[self.db_name]
+
+            
+
+    @classmethod
+    def generate_app(cls, app_dir, loud=False):
+        template_dir = os.path.normpath(os.path.join(os.path.dirname(__file__),
+                'app-template'))
+        shutil.copytree(template_dir, app_dir)
+
+    def push_app(self, app_dir, app_name):
+        docid = '_design/%s' % app_name
+
+        attach_dir = os.path.join(app_dir, '_attachments')
+
+        doc = self.dir_to_fields(app_dir)
+
+        if docid in self.db:
+            design = self.db[docid]
+            design.update(doc)
+            self.db[docid] = design
+        else:
+            self.db[docid] = doc 
+
+
+        self.push_directory(attach_dir, docid)
+
+    def _load_file(self, fname):
+        f = file(fname, 'r')
+        data = f.read()
+        f.close
+        return data
+
+    def dir_to_fields(self, app_dir, depth=0):
+        fields={}
+        for name in os.listdir(app_dir):
+            current_path = os.path.join(app_dir, name)
+            if name.startswith('.'):
+                continue
+            elif depth == 0 and name.startswith('_'):
+                continue
+            elif os.path.isdir(current_path):
+                fields[name] = self.dir_to_fields(current_path,
+                        depth=depth+1)
+            else:
+                content = self._load_file(current_path)
+                if name.endswith('.json'):
+                    fields[name] = json.loads(content)
+                elif name.endswith('.js'):
+                    fields[name[:-3]] = content
+                else:
+                    fields[name] = content
+        return fields
+    
+    def push_directory(self, attach_dir, docid):
+        design = self.db[docid]
+
+        signatures = {}
+        for root, dirs, files in os.walk(attach_dir):
+            if files:
+                for filename in files:
+                    file_path = os.path.join(root, filename)
+                    file = open(file_path, 'rb')
+                    name = file_path.split('%s/' % attach_dir)[1] 
+                    signatures[name] = _md5(file_path).hexdigest()
+                    self.db.put_attachment(design, file, name)
+        
+        # attachments to be removed
+        if 'signatures' in design:
+            for filename in design['signatures'].iterkeys():
+                if filename not in signatures:
+                    print filename
+                    self.db.delete_attachment(design, filename)
+        
+        design = self.db[docid]
+        design.update({'signatures': signatures})
+        self.db[docid] = design
---
+++ setup.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 Benoit Chesneau <benoitc@e-engura.org>
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+import os
+import sys
+import shutil
+
+from setuptools import setup
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+packages, data_files = [], []
+root_dir = os.path.join(os.path.dirname(__file__), 'python')
+if root_dir != '':
+    os.chdir(root_dir)
+
+couchapp_dir = 'couchapp'
+
+for dirpath, dirnames, filenames in os.walk(couchapp_dir):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+# add templates to dir
+
+for dirpath, dirnames, filenames in os.walk('app-template'):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+
+
+
+
+setup(
+    name = 'Couchapp',
+    version = '0.1.4',
+    url = 'http://github.com/benoitc/couchapp/tree/master',
+    license =  'Apache License 2',
+    author = 'Benoit Chesneau',
+    author_email = 'benoitc@e-engura.org',
+    description = 'Standalone CouchDB Application Development Made Simple.',
+    long_description = """CouchApp is a set of helpers and a jQuery plugin
+    that conspire to get you up and running on CouchDB quickly and
+    correctly. It brings clarity and order to the freedom of CouchDB’s
+    document-based approach.""",
+    keywords = 'couchdb couchapp',
+    platforms = 'any',
+    zip_safe = False,
+    scripts = ['couchapp/bin/couchapp'],
+    packages= packages,
+    data_files = data_files,
+    include_package_data = True,
+    classifiers = [
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Database',
+        'Topic :: Utilities',
+    ],
+
+    setup_requires = [
+        'setuptools',
+        'couchdb',
+    ]
+)
+
