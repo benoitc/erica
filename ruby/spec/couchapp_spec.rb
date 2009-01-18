63cf6f8ab2869413c9d80ffa1263b4c6b531c489
Chris Anderson <jchris@grabb.it>
2009-01-17 16:24:52 -0800

    faster specs

--- ruby/spec/couchapp_spec.rb
+++ ruby/spec/couchapp_spec.rb
@@ -17,26 +17,27 @@ describe "couchapp" do
   describe "generate my-app" do
     before(:all) do
       `#{@run} generate my-app`.should match(/generating/i)      
+      @files = Dir["#{@fixdir}/my-app/**/*"]
     end
     it "should create an app directory" do
-      Dir["#{@fixdir}/*"].select{|x|x =~ /my-app/}.length.should == 1
+      @files.select{|x|x =~ /my-app/}.should_not be_empty
     end
     it "should create a views directory" do
-      Dir["#{@fixdir}/my-app/*"].select{|x|x =~ /views/}.length.should == 1
+      @files.select{|x|x =~ /views\Z/}.length.should == 1
     end
     it "should create an _attachments directory" do
-      Dir["#{@fixdir}/my-app/*"].select{|x|x =~ /_attachments/}.length.should == 1
-      Dir["#{@fixdir}/my-app/_attachments/*"].select{|x|x =~ /index.html/}.length.should == 1
+      @files.select{|x|x =~ /_attachments\Z/}.length.should == 1
+      @files.select{|x|x =~ /_attachments\/index.html/}.length.should == 1
     end
     it "should create a show directory" do
-      Dir["#{@fixdir}/my-app/*"].select{|x|x =~ /show/}.length.should == 1
+      @files.select{|x|x =~ /show\Z/}.length.should == 1
     end
     it "should create a forms and libs" do
-      Dir["#{@fixdir}/my-app/show/docs/*"].select{|x|x =~ /example-show.js/}.length.should == 1
-      Dir["#{@fixdir}/my-app/lib/templates/*"].select{|x|x =~ /example.html/}.length.should == 1
+      @files.select{|x|x =~ /example-show.js/}.length.should == 1
+      @files.select{|x|x =~ /example.html/}.length.should == 1
     end
     it "should show deep attachment capabilities" do
-      Dir["#{@fixdir}/my-app/_attachments/**/*"].select{|x|x =~ /main.css/}.
+      @files.select{|x|x =~ /main.css/}.
         first.should include('style')
     end
   end
@@ -48,6 +49,8 @@ describe "couchapp" do
       @db.delete! rescue nil
       @db = @cr.create_db(TESTDB) rescue nil
       `#{@run} generate my-app`
+      `mkdir -p #{@fixdir}/my-app/views/more`
+      `echo 'moremap' > #{@fixdir}/my-app/views/more/map.js`
       `#{@run} push my-app #{TESTDB}`
       @doc = @db.get("_design/my-app")
     end
@@ -62,9 +65,6 @@ describe "couchapp" do
       @doc['show']['docs']['example-show'].should_not match(/\"helpers\"/)
     end
     it "should create view for all the views" do
-      `mkdir -p #{@fixdir}/my-app/views/more`
-      `echo 'moremap' > #{@fixdir}/my-app/views/more/map.js`
-      `#{@run} push my-app #{TESTDB}`
       doc = @db.get("_design/my-app")
       doc['views']['more']['map'].should match(/moremap/)
     end
@@ -81,21 +81,26 @@ describe "couchapp" do
       @doc['show']['docs']['example-show'].should_not match(/\"template\"/)
       @doc['show']['docs']['example-show'].should match(/Resig/)
     end
-  end
-
-  describe "push . #{TESTDB}" do
-    before(:all) do
-      @cr = CouchRest.new(COUCHHOST)
-      @db = @cr.database(TESTDB)
-      @db.delete! rescue nil
-      @db = @cr.create_db(TESTDB) rescue nil
-      `#{@run} generate my-app`
-    end
-    it "should create the design document" do
+    
+    it "should work on . dir as well" do
+      @db = reset_test_db!
       `cd #{@fixdir}/my-app && #{COUCHAPP} push . #{TESTDB}`
       lambda{@db.get("_design/my-app")}.should_not raise_error
     end
+    
   end
+
+  # describe "push . #{TESTDB}" do
+  #   before(:all) do
+  #     @cr = CouchRest.new(COUCHHOST)
+  #     @db = @cr.database(TESTDB)
+  #     @db.delete! rescue nil
+  #     @db = @cr.create_db(TESTDB) rescue nil
+  #     `#{@run} generate my-app`
+  #   end
+  #   it "should create the design document" do
+  #   end
+  # end
   
   describe "push my-app my-design #{TESTDB}" do
     before(:all) do
