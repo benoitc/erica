11a35b0c37eedd7a94463602832980f78f467c14
benoitc <bchesneau@gmail.com>
2009-01-25 21:51:49 +0100

    0.1.8 release

--- python/couchapp/__init__.py
+++ python/couchapp/__init__.py
@@ -6,6 +6,6 @@
 # This software is licensed as described in the file LICENSE, which
 # you should have received as part of this distribution.
 
-__VERSION__ = '0.1.7'
+__VERSION__ = '0.1.8'
 
 from couchapp.file_manager import FileManager
