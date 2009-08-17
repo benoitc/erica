8c3d2a50b06d5c6b8a7f618813010ec22219e476
Benoit Chesneau <bchesneau@gmail.com>
2009-08-17 22:00:37 +0200

    patch from @jasondavies. thanks!

--- src/couchapp/app.py
+++ src/couchapp/app.py
@@ -82,6 +82,7 @@ class CouchApp(object):
             '_attachments',
             'lists',
             'shows',
+            'updates',
             'views'
         ]
         
@@ -489,7 +490,9 @@ class CouchApp(object):
         if 'lists' in design_doc:
             package_shows(design_doc, design_doc['lists'], self.app_dir, objects, self.ui)
         
-
+        if 'updates' in design_doc:
+            package_shows(design_doc, design_doc['updates'], self.app_dir, objects, self.ui)
+            
         if 'validate_doc_update' in design_doc:
             tmp_dict = dict(validate_doc_update=design_doc["validate_doc_update"])
             package_shows(design_doc, tmp_dict, self.app_dir, objects, self.ui)
