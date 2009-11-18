0f0eae608b0d55ffb8179ae0816d97d8d8a671d0
Benoit Chesneau <bchesneau@gmail.com>
2009-11-18 03:28:30 +0100

    couchapp support only python >= 2.5

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@
 import os
 import sys
 
+if not hasattr(sys, 'version_info') or sys.version_info < (2, 5, 0, 'final'):
+    raise SystemExit("Couchapp requires Python 2.5 or later.")
+
 from distribute_setup import use_setuptools
 use_setuptools()
 from setuptools import setup, find_packages
