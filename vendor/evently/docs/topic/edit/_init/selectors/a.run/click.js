4a29a01fe741ffdd66f8fda676ea874a5e36088c
Chris Anderson <jchris@apache.org>
2010-02-16 13:56:34 -0800

    catch more agressively

--- vendor/evently/docs/topic/edit/_init/selectors/a.run/click.js
+++ vendor/evently/docs/topic/edit/_init/selectors/a.run/click.js
@@ -1,13 +1,22 @@
 function(e) {
-  e.preventDefault();
-  var id = e.data.args[1];
-  var example = $("#code-"+id);
-  var js = $('textarea',example).val() || $('pre',example).text();
-  $('#'+id).unbind();
   try {
-    eval(js);            
-  } catch (e) {
-    $('#'+id).html('<p>Error running #'+id+' code block:</p><p><pre>'+e.toSource()+'</pre></p>');
+    function err(y, id) {
+      $('#'+id).html(['<p>Error running #', id, 
+      ' code block:</p><p><pre>',
+      (y.toSource ? y.toSource() : JSON.stringify(y)),
+      '</pre></p>'].join(''));
+    }
+    var id = e.data.args[1];
+    var example = $("#code-"+id);
+    var js = $('textarea',example).val() || $('pre',example).text();
+    $('#'+id).unbind();
+    try {
+      eval(js);            
+    } catch (y) {
+      err(y, id);
+    }
+  } catch(x) {
+    err(x, id);
   }
   return false;
 }
