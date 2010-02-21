31114f76649958d78a96ec9068a8cd4adf3d9970
benoitc <bchesneau@gmail.com>
2010-02-21 18:40:16 +0100

    display an error on windows when py2exe isn't installed.

--- setup.py
+++ setup.py
@@ -24,26 +24,28 @@ for root in ('templates', 'vendor'):
 
 
 
-# py2exe needs to be installed to work
-try:
-    import py2exe
 
-    # Help py2exe to find win32com.shell
+if os.name == "nt":
+    # py2exe needs to be installed to work
     try:
-        import modulefinder
-        import win32com
-        for p in win32com.__path__[1:]: # Take the path to win32comext
-            modulefinder.AddPackagePath("win32com", p)
-        pn = "win32com.shell"
-        __import__(pn)
-        m = sys.modules[pn]
-        for p in m.__path__[1:]:
-            modulefinder.AddPackagePath(pn, p)
-    except ImportError:
-        pass
+        import py2exe
+
+        # Help py2exe to find win32com.shell
+        try:
+            import modulefinder
+            import win32com
+            for p in win32com.__path__[1:]: # Take the path to win32comext
+                modulefinder.AddPackagePath("win32com", p)
+            pn = "win32com.shell"
+            __import__(pn)
+            m = sys.modules[pn]
+            for p in m.__path__[1:]:
+                modulefinder.AddPackagePath(pn, p)
+        except ImportError:
+            pass
     
-except ImportError:
-    pass
+    except ImportError:
+        raise SystemExit('You need py2exe installed to run Couchapp.')
     
 
 class install_package_data(install_data):
