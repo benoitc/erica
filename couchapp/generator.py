357278595c7de74d2f40926ea7089496316deda4
benoitc <bchesneau@gmail.com>
2010-03-24 17:20:49 +0100

    fix error spotted by nrstott on irc. thanks!

--- couchapp/generator.py
+++ couchapp/generator.py
@@ -3,6 +3,8 @@
 # This file is part of couchapp released under the Apache 2 license. 
 # See the NOTICE for more information.
 
+from __future__ import with_statement
+
 import logging
 import os
 import shutil
