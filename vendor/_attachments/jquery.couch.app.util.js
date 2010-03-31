9d26dbf2dc2941ce55d45061194ced4b85c09544
Chris Anderson <jchris@apache.org>
2010-03-31 14:49:21 -0700

    add splat to evently and pathbinder

--- vendor/_attachments/jquery.couch.app.util.js
+++ vendor/_attachments/jquery.couch.app.util.js
@@ -1,6 +1,6 @@
-$.log = function() {
+$.log = function(m) {
   if (window && window.console && window.console.log) {
-    window.console.log(arguments);
+    window.console.log(arguments.length == 1 ? m : arguments);
   }
 };
 
@@ -55,3 +55,11 @@ $.prettyDate = function(time){
 		day_diff < 730 && Math.ceil( day_diff / 31 ) + " months ago" ||
 		Math.ceil( day_diff / 365 ) + " years ago";
 };
+
+$.toArray = function(args) {
+  var array = [];
+  for (var i=0; i < args.length; i++) {
+    array.push(args[i]);
+  };
+  return array;
+}
