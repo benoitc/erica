6ffc07902c0c41a88448d18dca105b3cbb0b49cf
Jarrod Roberson <jarrod@vertigrated.com>
2010-04-28 23:05:16 -0400

    adding compatiblity code so that jquery.couch.app.js will work in IE7/IE8

--- vendor/_attachments/jquery.couch.app.js
+++ vendor/_attachments/jquery.couch.app.js
@@ -233,3 +233,45 @@
   // legacy support. $.CouchApp is deprecated, please use $.couch.app
   $.CouchApp = $.couch.app;
 })(jQuery);
+
+// JavaScript 1.6 compatibility functions that are missing from IE7/IE8
+
+if (!Array.prototype.forEach)
+{
+    Array.prototype.forEach = function(fun /*, thisp*/)
+    {
+        var len = this.length >>> 0;
+        if (typeof fun != "function")
+            throw new TypeError();
+
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++)
+        {
+            if (i in this)
+                fun.call(thisp, this[i], i, this);
+        }
+    };
+}
+
+if (!Array.prototype.indexOf)
+{
+    Array.prototype.indexOf = function(elt)
+    {
+        var len = this.length >>> 0;
+
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0)
+                ? Math.ceil(from)
+                : Math.floor(from);
+        if (from < 0)
+            from += len;
+
+        for (; from < len; from++)
+        {
+            if (from in this &&
+                this[from] === elt)
+                return from;
+        }
+        return -1;
+    };
+}
