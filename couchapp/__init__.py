eecd2b80e5b9e385c3c85fbb8e2a627e3a556c93
benoitc <bchesneau@gmail.com>
2010-06-05 20:18:28 +0200

    bump to 0.6.2

--- couchapp/__init__.py
+++ couchapp/__init__.py
@@ -3,5 +3,5 @@
 # This file is part of couchapp released under the Apache 2 license. 
 # See the NOTICE for more information.
 
-version_info = (0, 6, "2-dev")
+version_info = (0, 6, 2)
 __version__ =  ".".join(map(str, version_info))
