fc79f3ce612efb210e661dcdf216caf0fb97e292
benoitc <bchesneau@gmail.comwq>
2010-12-01 22:03:32 +0100

    put back the legacy macros system.

--- src/couchapp_push.erl
+++ src/couchapp_push.erl
@@ -159,6 +159,7 @@ send_attachments(Db, #couchapp{att_dir=AttDir, doc=Doc,
 
 make_doc(Couchapp) ->
     #couchapp{
+        path=AppDir,
         doc=Doc,
         old_doc=OldDoc,
         manifest=Manifest,
@@ -185,15 +186,175 @@ make_doc(Couchapp) ->
                 Signatures, Meta1),
             couchbeam_doc:set_value(<<"couchapp">>, FinalMeta, Doc1)
     end,
-    process_macros(Doc2).
-    
+    process_macros(Doc2, AppDir).
+
+process_macros(Doc, AppDir) ->
+    Funs = [<<"shows">>, <<"lists">>, <<"updates">>, <<"filters">>, 
+        <<"spatial">>, <<"validate_update_doc">>],
+    %% apply macros too functions
+    Doc1 = process_macros_fun(Funs, Doc, Doc, AppDir),
+    Doc2 = case couchbeam_doc:get_value(<<"views">>, Doc1) of
+        undefined ->
+            Doc1;
+        {Views} -> 
+            %% process macros in views
+            Views1 = lists:foldl(fun({ViewName, View}, Att) ->
+                        View1= process_macros_fun([<<"map">>, <<"reduce">>], 
+                            View, Doc1, AppDir),
+                        [{ViewName, View1}|Att]
+                        end, [], Views),
+            couchbeam_doc:set_value(<<"views">>, {Views1}, Doc1)
+    end,
+    Doc2.
+
 %% ===================================================================
 %% Internal functions
 %% ===================================================================
 
-process_macros(Doc) ->
-    %% do nothing for now.
-    Doc.
+process_macros_fun([], Obj, _Doc, _AppDir) ->
+    Obj;
+process_macros_fun([Prop|Rest], Obj, Doc, AppDir) ->
+    case couchbeam_doc:get_value(Prop, Obj) of
+        undefined ->
+            process_macros_fun(Rest, Obj, Doc, AppDir);
+        Sources ->
+            ?DEBUG("process function ~p~n", [Prop]),
+            Sources1 = lists:foldl(fun(Source, Acc) ->
+                    Source1 = apply_macros(Source, Doc, AppDir),
+                    [Source1|Acc]
+                end, [], Sources),
+            Obj1 = couchbeam_doc:set_value(Prop, Sources1, Obj),
+            process_macros_fun(Rest, Obj1, Doc, AppDir)
+    end.
+
+apply_macros(Source, Doc, AppDir) ->
+    Source1 = apply_code_macros(Source, AppDir),
+    apply_json_macros(Source1, Doc, AppDir).
+
+apply_code_macros(Source, AppDir) ->
+    case re:run(Source, "^\s*\/\/\#\ ?!code (.*)$", 
+            [global, caseless, unicode, multiline, 
+                {capture, all, binary}]) of
+        nomatch -> Source;
+        {match, Matches} ->
+            apply_code_macros1(Matches, Source, AppDir)
+    end.
+
+apply_code_macros1([], Source, _AppDir) ->
+    Source;
+apply_code_macros1([Match|Rest], Source, AppDir) ->
+    [Replacement, Path] = Match,
+    Path1 = filename:join(Path, Path),
+    Contents = lists:foldl(fun(File, Acc) ->
+                case file:read_file(File) of
+                    {ok, Bin} ->
+                        Bin1 = apply_code_macros(Bin, AppDir),
+                        [Bin1|Acc];
+                    Error ->
+                        ?ERROR("macro: can't read ~p, [~p]~n", [File,
+                                Error]),
+                        Acc
+                end
+        end, [], filelib:wildcard(Path1)), 
+    Content = iolist_to_binary(list:reverse(Contents)),
+    Source1 = re:replace(Source, Replacement, Content, [global, caseless, 
+            multiline, {return, binary}]),
+    apply_code_macros1(Rest, Source1, AppDir).
+
+apply_json_macros(Source, Doc, AppDir) ->
+    case re:run(Source, "^\s*\/\/\#\ ?!json (.*)$", 
+            [global, caseless, unicode, multiline, 
+                {capture, all, binary}]) of
+        nomatch -> Source;
+        {match, Matches} ->
+            apply_json_macros1(Matches, Source, Doc, AppDir)
+    end.
+
+apply_json_macros1([], Source, _Doc, _AppDir) ->
+    Source;
+apply_json_macros1([Match|Rest], Source, Doc, AppDir) ->
+    [Replacement, JsonPath] = Match,
+    Content = case JsonPath of
+        <<"_attachments", _/binary>> ->
+            JsonPath1 = binary_to_list(JsonPath),
+            Path = filename:join(AppDir, JsonPath1),
+            Values = lists:foldl(fun(File, Att) ->
+                case file:file_read(File) of 
+                    {ok, Bin} ->
+                        {Path1, Value} = case filename:extension(File) of
+                            ".json" ->
+                                Bin1 =  couchbeam:json_decode(Bin),
+                                {filename:basename(JsonPath1,
+                                        ".json"), Bin1};
+                            [] ->
+                                {JsonPath1, Bin};
+                            Ext ->
+                                {filename:basename(JsonPath1, Ext),
+                                    Bin} 
+                        end,
+                        [_,VarName|Fields] = string:tokens(Path1, "/"),
+                        [{VarName, nested_value(Fields, Value)}|Att];
+                    Error ->
+                        ?ERROR("macro: can't read ~p, [~p]~n", [File,
+                            Error]),
+                        Att
+                end
+            end, [], filelib:wildcard(Path)),
+            lists:flatten(["var _attachments = ", 
+                    couchbeam_util:json_encode({Values})]);
+        _ ->
+            Props = string:tokens(binary_to_list(JsonPath), "."),
+            case get_value(Props, Doc) of
+                {ok, Value} ->
+                    [VarName|Fields] = Props,
+                    Value1 = nested_value(Fields, Value),
+                    lists:flatten(["var ", VarName, " = ",
+                            couchbeam_util:json_encode(Value1)]);
+                _Error ->
+                    ?ERROR("json macro: can't find ~p~n", [JsonPath]),
+                    ""
+            end
+    end,
+    Source1 = re:replace(Source, Replacement, Content, [global, caseless, 
+            multiline, {return, binary}]),
+    apply_json_macros1(Rest, Source1, Doc, AppDir).
+        
+nested_value(Fields, Value) ->
+    case Fields of
+        [] ->
+            Value;
+        _ ->
+            nested_value1(Fields, Value, length(Fields), 1)
+    end.
+
+nested_value1([Field|Rest], Value, Len, Count) when Count < length(Rest) ->
+    couchbeam_doc:set_value(Field, 
+        nested_value1(Rest, Value, Len, Count+1), {[]});
+nested_value1([Field|_], Value, _Len, _Count) ->
+    {Field, Value}.
+
+
+get_value(Props, Doc) ->
+    get_value(Props, Doc, length(Props), 1).
+
+get_value([Name|Rest], Obj, Len, Count) when Count < Len ->
+    case couchbeam_doc:get_value(Name, Obj) of
+        undefined ->
+            not_found;
+        {[_]}=Value ->
+            couchbeam_doc:get_value(Rest, Value, Value, Len, Count + 1);
+        _ ->
+            not_found
+    end;
+get_value([Name|_], Obj, _Len, _Count) ->
+   case couchbeam_doc:get_value(Name, Obj) of
+       undefined ->
+           not_found;
+       Value ->
+           {ok, Value}
+    end.
+
+
 
 clean_old_attachments([],OldAtts) ->
     OldAtts;    
